a24090d44daa2b09552b613923004dd31984321a
Manuel Alvarez <manuel.am2003@gmail.com>
2024-02-28 17:20:34 +0100

    Set up barebones routes and controllers

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
+const compression = require("compression");
+const helmet = require("helmet");
 
 const app = express();
 
@@ -15,6 +16,11 @@ mongoose.set("strictQuery", false);
 const mongoDB =
   "mongodb+srv://manuelam2003:9n0VETa6YAAWdMQS@cluster0.fuhvhlm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
@@ -23,15 +29,26 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 app.use(express.static(path.join(__dirname, "public")));
 
-main().catch((err) => console.log(err));
-async function main() {
-  await mongoose.connect(mongoDB);
-}
-
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
